521fbce930ae2a27f462b9d512a42f6c5db7b5aa
RussellStauffer <tspendragn@yahoo.com>
2017-04-28 11:13:09 -0700

    Add files via upload
    This is a fun little investment game based on real-world models. It teaches considering risk to reward in your investment choices.
    Python version is 3.6.1, but if you use Python 2.7, it may not run until you change the input statements to raw_input statements. Enjoy!

---
+++ InvestmentGame.py
@@ -0,0 +1,277 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: E:\Python\Section36A.py ======================
+   The Retirement Game
+
+What is your name: Russ
+Hello, Russ welcome to 
+   The Retirement Game
+
+In this game Russ you must excel at saving for retirement
+The average retiree has saved for 30 years
+With an average yield per year of 5%
+If they save $100 a month during that period, they will have
+saved nearly $  83712.95
+
+Russ,Do you think you can do better?
+Enter y (Yes) or n(No)y
+You're a brave person, Russ!
+
+What kind of investor are you?
+
+1 - Do you like to take risks to get great rewards?
+2 - Are you very cautious with your money?
+3- Do you prefer a little bit of risk for higher rewards?
+
+Enter 1, 2, or 3:   1
+You would do best with stocks
+
+You can choose from the following stocks:
+
+
+Here are the stocks you can choose from. Remember that the higher the beta,
+ the higher the risk, or potential rewards
+
+
+    Name                  Symbol  Beta  Price
+   1 U.S.Steel               X    4.08   20.0
+   2 Tesoro                  TSO  1.21   50.0
+   3 Pacific Gas & Electric  PCG  0.16   30.0
+   4 Testa                   TSLA 1.14   200.0
+   5 Ford Motor              F    1.36   10.0
+
+
+Choose a stock using the number beside the name (1-5)1
+/nYou have selected  U.S.Steel              
+Bankruptcy. Company reorganizes. The new stock price is 15.84
+Bankruptcy. Company reorganizes. The new stock price is 22.68
+Bankruptcy. Company reorganizes. The new stock price is 19.39
+You own  294  shares of  U.S.Steel              
+each share is worth $  4.08
+
+Your final worth is: $  1200.0
+
+Do you want to try again? (y/n) y
+You're a brave person, Russ!
+
+What kind of investor are you?
+
+1 - Do you like to take risks to get great rewards?
+2 - Are you very cautious with your money?
+3- Do you prefer a little bit of risk for higher rewards?
+
+Enter 1, 2, or 3:   1
+You would do best with stocks
+
+You can choose from the following stocks:
+
+
+Here are the stocks you can choose from. Remember that the higher the beta,
+ the higher the risk, or potential rewards
+
+
+    Name                  Symbol  Beta  Price
+   1 U.S.Steel               X    4.08   20.0
+   2 Tesoro                  TSO  1.21   50.0
+   3 Pacific Gas & Electric  PCG  0.16   30.0
+   4 Testa                   TSLA 1.14   200.0
+   5 Ford Motor              F    1.36   10.0
+
+
+Choose a stock using the number beside the name (1-5)2
+/nYou have selected  Tesoro                 
+You own  335  shares of  Tesoro                 
+each share is worth $  2777.08
+
+Your final worth is: $  929184.94
+
+Do you want to try again? (y/n) y
+You're a brave person, Russ!
+
+What kind of investor are you?
+
+1 - Do you like to take risks to get great rewards?
+2 - Are you very cautious with your money?
+3- Do you prefer a little bit of risk for higher rewards?
+
+Enter 1, 2, or 3:   1
+You would do best with stocks
+
+You can choose from the following stocks:
+
+
+Here are the stocks you can choose from. Remember that the higher the beta,
+ the higher the risk, or potential rewards
+
+
+    Name                  Symbol  Beta  Price
+   1 U.S.Steel               X    4.08   20.0
+   2 Tesoro                  TSO  1.21   50.0
+   3 Pacific Gas & Electric  PCG  0.16   30.0
+   4 Testa                   TSLA 1.14   200.0
+   5 Ford Motor              F    1.36   10.0
+
+
+Choose a stock using the number beside the name (1-5)3
+/nYou have selected  Pacific Gas & Electric 
+You own  965  shares of  Pacific Gas & Electric 
+each share is worth $  37.92
+
+Your final worth is: $  36572.88
+
+Do you want to try again? (y/n) y
+You're a brave person, Russ!
+
+What kind of investor are you?
+
+1 - Do you like to take risks to get great rewards?
+2 - Are you very cautious with your money?
+3- Do you prefer a little bit of risk for higher rewards?
+
+Enter 1, 2, or 3:   1
+You would do best with stocks
+
+You can choose from the following stocks:
+
+
+Here are the stocks you can choose from. Remember that the higher the beta,
+ the higher the risk, or potential rewards
+
+
+    Name                  Symbol  Beta  Price
+   1 U.S.Steel               X    4.08   20.0
+   2 Tesoro                  TSO  1.21   50.0
+   3 Pacific Gas & Electric  PCG  0.16   30.0
+   4 Testa                   TSLA 1.14   200.0
+   5 Ford Motor              F    1.36   10.0
+
+
+Choose a stock using the number beside the name (1-5)4
+/nYou have selected  Testa                  
+You own  101  shares of  Testa                  
+each share is worth $  1363.04
+
+Your final worth is: $  137998.38
+
+Do you want to try again? (y/n) y
+You're a brave person, Russ!
+
+What kind of investor are you?
+
+1 - Do you like to take risks to get great rewards?
+2 - Are you very cautious with your money?
+3- Do you prefer a little bit of risk for higher rewards?
+
+Enter 1, 2, or 3:   1
+You would do best with stocks
+
+You can choose from the following stocks:
+
+
+Here are the stocks you can choose from. Remember that the higher the beta,
+ the higher the risk, or potential rewards
+
+
+    Name                  Symbol  Beta  Price
+   1 U.S.Steel               X    4.08   20.0
+   2 Tesoro                  TSO  1.21   50.0
+   3 Pacific Gas & Electric  PCG  0.16   30.0
+   4 Testa                   TSLA 1.14   200.0
+   5 Ford Motor              F    1.36   10.0
+
+
+Choose a stock using the number beside the name (1-5)5
+/nYou have selected  Ford Motor             
+You own  1329  shares of  Ford Motor             
+each share is worth $  89.2
+
+Your final worth is: $  118497.82
+
+Do you want to try again? (y/n) y
+You're a brave person, Russ!
+
+What kind of investor are you?
+
+1 - Do you like to take risks to get great rewards?
+2 - Are you very cautious with your money?
+3- Do you prefer a little bit of risk for higher rewards?
+
+Enter 1, 2, or 3:   2
+I'd just keep my money in the bank
+Bank Routine. This is still under development
+After 30 years of just sticking your money in the bank, you
+saved a total of $  38736.02
+
+Do you want to try again? (y/n) y
+You're a brave person, Russ!
+
+What kind of investor are you?
+
+1 - Do you like to take risks to get great rewards?
+2 - Are you very cautious with your money?
+3- Do you prefer a little bit of risk for higher rewards?
+
+Enter 1, 2, or 3:   3
+You should try investment grade bond funds
+
+Select one of these three bonds:
+
+Bond Name                              Rating
+1.  CALPERS 2040 - 3.5%                 BBB
+2.  City of Portland 2037 - 2%          AA+
+3.  State of Illinois 2038 - 5.5%       CCC
+
+Remember that the rating determines the safety. Bonds can go bankrupt!
+Enter your choice (1,2 or 3): 1
+
+you selected:  CALPERS 2040 - 3.5%  Not a bad choice
+In the end, you had  3  bonds worth $  46095.75
+and $ 4800.0 in cash
+for a net worth of $  50896
+
+Do you want to try again? (y/n) y
+You're a brave person, Russ!
+
+What kind of investor are you?
+
+1 - Do you like to take risks to get great rewards?
+2 - Are you very cautious with your money?
+3- Do you prefer a little bit of risk for higher rewards?
+
+Enter 1, 2, or 3:   2
+I'd just keep my money in the bank
+Bank Routine. This is still under development
+After 30 years of just sticking your money in the bank, you
+saved a total of $  38736.02
+
+Do you want to try again? (y/n) y
+You're a brave person, Russ!
+
+What kind of investor are you?
+
+1 - Do you like to take risks to get great rewards?
+2 - Are you very cautious with your money?
+3- Do you prefer a little bit of risk for higher rewards?
+
+Enter 1, 2, or 3:   3
+You should try investment grade bond funds
+
+Select one of these three bonds:
+
+Bond Name                              Rating
+1.  CALPERS 2040 - 3.5%                 BBB
+2.  City of Portland 2037 - 2%          AA+
+3.  State of Illinois 2038 - 5.5%       CCC
+
+Remember that the rating determines the safety. Bonds can go bankrupt!
+Enter your choice (1,2 or 3): 3
+
+You chose:  State of Illinois 2038 - 5.5%  Risky, a junk bond!
+In the end, you had  3  bonds worth $  59932.06
+and $ 4800.0 in cash
+for a net worth of $  64732
+
+Do you want to try again? (y/n) n
+See you later!
+>>> 
